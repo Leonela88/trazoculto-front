6588fd6b3c33ef12ecfa8906647a6d7b149616ed
Leonela88 <rivasaela@gmail.com>
2026-06-08 18:35:42 +0200

    style: improve layout proportions, responsive design and contact page spacing

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -3,14 +3,16 @@ import styles from "./footer.module.css"
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.left}>
-        <span>©</span>
-        <span className={styles.divider}>|</span>
-        <span>trazoculto</span>
-      </div>
-      <div className={styles.center}>2026</div>
-      <div className={styles.right}>
-        by Mariel <br /> Pannunzio
+      <div className={styles.footerInner}>
+        <div className={styles.left}>
+          <span>©</span>
+          <span className={styles.divider}>|</span>
+          <span>trazoculto</span>
+        </div>
+        <div className={styles.center}>2026</div>
+        <div className={styles.right}>
+          by Mariel <br /> Pannunzio
+        </div>
       </div>
     </footer>
   )
